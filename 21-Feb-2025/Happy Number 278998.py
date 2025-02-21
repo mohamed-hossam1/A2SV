2f43795156c31c453e5bebab54bc83ad2a10f2ed
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2025-02-21 18:25:22 +0200

    Created 'Happy Number' by A2SV Hub

---
+++ 21-Feb-2025/Happy Number 278998.py
@@ -0,0 +1,13 @@
+# Problem: Happy Number - https://leetcode.com/problems/happy-number/description/
+
+class Solution(object):
+    def isHappy(self, n):
+        mem = set()
+        while n != 1:
+            n = sum([int(i) ** 2 for i in str(n)])
+            if n in mem:
+                return False
+            else:
+                mem.add(n)
+        else:
+            return True
