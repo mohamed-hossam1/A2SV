c4ddad2588536c17da6feb8ea2b0e6478a4eb633
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2025-04-24 22:11:19 +0200

    Created 'Number of Recent Calls' by A2SV Hub

---
+++ 24-Apr-2025/Number of Recent Calls 341887.py
@@ -0,0 +1,12 @@
+# Problem: Number of Recent Calls - https://leetcode.com/problems/number-of-recent-calls/
+
+class RecentCounter:
+
+    def __init__(self):
+        self.q = deque()
+
+    def ping(self, t: int) -> int:
+        self.q.append(t)
+        while self.q[0] < t - 3000:
+            self.q.popleft()
+        return len(self.q)
