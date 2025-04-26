118dfe5b27523f5e953fb9f8d3734c6d161945fb
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2025-04-26 22:21:52 +0300

    Created 'Daily Temperatures' by A2SV Hub

---
+++ 26-Apr-2025/Daily Temperatures 343091.py
@@ -0,0 +1,17 @@
+# Problem: Daily Temperatures - https://leetcode.com/problems/daily-temperatures/
+
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        ans = [0]*len(temperatures)
+        for i in range(len(temperatures)-1,-1,-1):
+            while stack and temperatures[stack[-1]] <=temperatures[i]:
+                stack.pop()
+
+            if stack:
+                ans[i] = stack[-1]-i
+            
+            stack.append(i)
+        return ans
+
+        
