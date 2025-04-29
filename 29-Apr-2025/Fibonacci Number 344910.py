23239c37fcb8c5f710cb80ab0c78fc7c1fd8deac
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2025-04-29 21:27:02 +0300

    Created 'Fibonacci Number' by A2SV Hub

---
+++ 29-Apr-2025/Fibonacci Number 344910.py
@@ -0,0 +1,11 @@
+# Problem: Fibonacci Number - https://leetcode.com/problems/fibonacci-number/
+
+class Solution(object):
+    def fib(self, n):
+        if n==0:
+            return 0
+        elif n==1:
+            return 1
+        return self.fib(n-1) + self.fib(n-2)
+        
+
