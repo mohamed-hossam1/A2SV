d98ea28ee570136fe66d281cbd992634b440cbb2
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2025-04-30 20:43:09 +0300

    Created 'Pow (x, n)' by A2SV Hub

---
+++ 30-Apr-2025/Pow (x, n) 345882.py
@@ -0,0 +1,19 @@
+# Problem: Pow (x, n) - https://leetcode.com/problems/powx-n/
+
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n >= 0:
+            return self.helper(x, n)
+        else:
+            return 1/self.helper(x, -n)
+    
+    def helper(self, x, n): 
+        if n == 0:
+            return 1
+        
+        temp = self.myPow(x, n//2)
+         
+        if int(n%2) == 0:
+            return  temp * temp
+        else:
+            return temp * temp * x
