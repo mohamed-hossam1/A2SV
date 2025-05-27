a1dee32d7b842ef2ae67467fcfcd0b04ae096e4d
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2025-05-27 22:47:01 +0300

    Created 'Split string and Join' by A2SV Hub

---
+++ 27-May-2025/Split string and Join 360330.py
@@ -0,0 +1,5 @@
+# Problem: Split string and Join - https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true
+
+def split_and_join(line):
+    splited = line.split(" ")
+    return ("-".join(splited))
