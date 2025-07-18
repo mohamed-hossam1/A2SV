433b4a4430331fd9ba059ff5c69f31e1fed97e07
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2025-07-18 22:31:42 +0300

    Created 'Minimum Moves to Reach Target Score' by A2SV Hub

---
+++ 18-Jul-2025/Minimum Moves to Reach Target Score 377804.py
@@ -0,0 +1,19 @@
+# Problem: Minimum Moves to Reach Target Score - https://leetcode.com/problems/minimum-moves-to-reach-target-score/
+
+class Solution:
+    def minMoves(self, target: int, maxDoubles: int) -> int:
+        ans=0
+        while target!=1:
+            if target%2==0 and maxDoubles!=0: 
+                target=target/2
+                maxDoubles-=1
+                ans+=1                   
+            else:
+                if maxDoubles==0:
+                    ans=ans+(target-1)
+                    target=1 
+                else:
+                    target-=1
+                    ans+=1
+        return int(ans)
+        
