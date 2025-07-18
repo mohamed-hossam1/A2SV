39b22d5e2adaaeba01b1a0d001bbedc3d895d0b9
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2025-07-18 21:37:14 +0300

    Created 'Maximum Number of Consecutive Values You Can Make' by A2SV Hub

---
+++ 18-Jul-2025/Maximum Number of Consecutive Values You Can Make 377729.py
@@ -0,0 +1,11 @@
+# Problem: Maximum Number of Consecutive Values You Can Make - https://leetcode.com/problems/maximum-number-of-consecutive-values-you-can-make/description/
+
+class Solution:
+    def getMaximumConsecutive(self, coins: List[int]) -> int:
+        coins.sort()
+        res = 1
+        for coin in coins:
+            if (res >= coin):
+                res += coin
+        
+        return res
