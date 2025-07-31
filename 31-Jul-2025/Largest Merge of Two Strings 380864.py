c128b2568c4a5b7335fdc4f39e2cedc595d74c57
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2025-07-31 20:59:57 +0300

    Created 'Largest Merge of Two Strings' by A2SV Hub

---
+++ 31-Jul-2025/Largest Merge of Two Strings 380864.py
@@ -0,0 +1,9 @@
+# Problem: Largest Merge of Two Strings - https://leetcode.com/problems/largest-merge-of-two-strings/
+
+class Solution:
+    def largestMerge(self, s1, s2):
+        if s1 >= s2 > '':
+            return s1[0] + self.largestMerge(s1[1:], s2)
+        if s2 >= s1 > '':
+            return s2[0] + self.largestMerge(s1, s2[1:])
+        return s1 + s2
