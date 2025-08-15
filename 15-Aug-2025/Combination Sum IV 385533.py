eddec30e5abaf5f43453afe50520498858022492
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2025-08-15 21:05:03 +0300

    Created 'Combination Sum IV' by A2SV Hub

---
+++ 15-Aug-2025/Combination Sum IV 385533.py
@@ -0,0 +1,16 @@
+# Problem: Combination Sum IV - https://leetcode.com/problems/combination-sum-iv/description/
+
+class Solution:
+    def combinationSum4(self, nums: list[int], target: int) -> int:
+    
+        @lru_cache(None)
+        def dp(target: int, res = 0)->int:
+       
+            for n in nums:
+            
+                if n == target: res+= 1
+                if n <  target: res+= dp(target-n)
+
+            return res    
+
+        return  dp(target)
