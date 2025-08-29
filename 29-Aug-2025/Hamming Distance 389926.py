8181cd1359c98262ec67ca789d581e9c01f50c3d
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2025-08-29 21:17:47 +0300

    Created 'Hamming Distance' by A2SV Hub

---
+++ 29-Aug-2025/Hamming Distance 389926.py
@@ -0,0 +1,14 @@
+# Problem: Hamming Distance - https://leetcode.com/problems/hamming-distance/
+
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        cnt = 0
+        x, y = bin(x)[2:], bin(y)[2:]
+        x = max(0,len(y)-len(x))*"0" + x
+        y = max(0,len(x)-len(y))*"0" + y
+
+        for i in range(len(x)):
+            if x[i] != y[i]:
+                cnt += 1
+
+        return cnt        
