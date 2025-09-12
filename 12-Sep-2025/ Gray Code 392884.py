5eb09058b686c21afd8b7d3fed3c45e88b43431d
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2025-09-13 00:21:11 +0300

    Created ' Gray Code' by A2SV Hub

---
+++ 12-Sep-2025/ Gray Code 392884.py
@@ -0,0 +1,10 @@
+# Problem:  Gray Code - https://leetcode.com/problems/gray-code/description/
+
+class Solution:
+  def grayCode(self, n: int) -> list[int]:
+    ans = [0]
+    for i in range(n):
+      for j in reversed(range(len(ans))):
+        ans.append(ans[j] | 1 << i)
+
+    return ans
