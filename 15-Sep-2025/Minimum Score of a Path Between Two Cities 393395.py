16e72d3a7e780053f01b1bf3af696dedb8f621a9
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2025-09-16 00:37:40 +0300

    Created 'Minimum Score of a Path Between Two Cities' by A2SV Hub

---
+++ 15-Sep-2025/Minimum Score of a Path Between Two Cities 393395.py
@@ -0,0 +1,20 @@
+# Problem: Minimum Score of a Path Between Two Cities - https://leetcode.com/problems/minimum-score-of-a-path-between-two-cities/description/
+
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        ht = defaultdict(list)
+        for i,j,dis in roads:
+            ht[i].append(j)
+            ht[j].append(i)
+        ans,view= inf,set()
+        queue = deque([1])
+        while queue:
+            cur_city = queue.popleft()
+            for next_city in ht[cur_city]:
+                if next_city not in view:
+                    queue.append(next_city)
+                    view.add(next_city)
+        for i,j,dis in roads:
+            if i in view or j in view:
+                ans=min(ans,dis)
+        return ans
