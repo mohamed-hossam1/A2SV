da93a9dd380224eecc8e00bffbd1c142f364e865
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2025-09-24 00:26:58 +0300

    Created 'Map Sum Pairs' by A2SV Hub

---
+++ 23-Sep-2025/Map Sum Pairs 395419.py
@@ -0,0 +1,18 @@
+# Problem: Map Sum Pairs - https://leetcode.com/problems/map-sum-pairs/description/
+
+class MapSum:
+
+    def __init__(self):
+        
+
+    def insert(self, key: str, val: int) -> None:
+        
+
+    def sum(self, prefix: str) -> int:
+        
+
+
+# Your MapSum object will be instantiated and called as such:
+# obj = MapSum()
+# obj.insert(key,val)
+# param_2 = obj.sum(prefix)
