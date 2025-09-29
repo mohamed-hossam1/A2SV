729cdd62f014ec3a2c9f553b02209115c11aa069
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2025-09-29 22:11:18 +0300

    Created 'Short Encoding of Words' by A2SV Hub

---
+++ 29-Sep-2025/Short Encoding of Words 397498.py
@@ -0,0 +1,14 @@
+# Problem: Short Encoding of Words - https://leetcode.com/problems/short-encoding-of-words/
+
+class Solution:                         
+    def minimumLengthEncoding(self, words: List[str]) -> int:
+
+        words = sorted(map(lambda x: x[::-1], words))
+
+        ans = len(words[-1])+1
+
+        for i in range(len(words)-1):
+            if not words[i+1].startswith(words[i]):
+                ans+= len(words[i])+1  
+
+        return ans
