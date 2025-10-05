c4e697cc0f34f3c13d7fec9f990b53b7dba1331a
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2025-10-06 00:36:41 +0300

    Created 'Single Number' by A2SV Hub

---
+++ 05-Oct-2025/Single Number 400138.py
@@ -0,0 +1,9 @@
+# Problem: Single Number - https://leetcode.com/problems/single-number/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        xor = 0
+        for num in nums:
+            xor ^= num
+        
+        return xor
