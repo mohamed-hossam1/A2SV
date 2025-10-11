84486e04fd5010994bbb25a44a1706a5f98511df
mohamed-hossam1 <mohamedhossamv8@gmail.com>
2025-10-11 22:28:17 +0300

    Created 'Number of Ways to Arrive at Destination' by A2SV Hub

---
+++ 11-Oct-2025/Number of Ways to Arrive at Destination 402211.py
@@ -0,0 +1,34 @@
+# Problem: Number of Ways to Arrive at Destination - https://leetcode.com/problems/number-of-ways-to-arrive-at-destination/
+
+import heapq
+
+MOD = 1000000007
+
+class Solution:
+    def countPaths(self, n, roads):
+        adj = [[] for _ in range(n)]
+        
+        for u, v, wt in roads:
+            adj[u].append((wt, v))
+            adj[v].append((wt, u))
+        
+        dist = [float('inf')] * n
+        dist[0] = 0
+        
+        ways = [0] * n
+        ways[0] = 1
+        
+        pq = [(0, 0)]  
+        
+        while pq:
+            wt, node = heapq.heappop(pq)
+            
+            for newWt, newNode in adj[node]:
+                if wt + newWt < dist[newNode]:
+                    dist[newNode] = wt + newWt
+                    heapq.heappush(pq, (dist[newNode], newNode))
+                    ways[newNode] = ways[node]
+                elif wt + newWt == dist[newNode]:
+                    ways[newNode] = (ways[newNode] + ways[node]) % MOD
+        
+        return int(ways[n - 1] % MOD)
